345f4d862726c4b5a092dfefe6d2f4ef8e2bc77b
Rafael Calhau <rafael@calhau.me>
2021-10-20 20:54:05 -0300

    added cognito condigurations

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_LQTnDV9UM', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '49kegiela7rbe7fee1rfgtbelm', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
